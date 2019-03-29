e6e44dfeb241d33fd741796467fc9916d52d865f
Tanvir Shahriar <tanvir.shahriar@mail.utoronto.ca>
2019-03-29 02:34:32 -0400

    Improvements to ball mechanics

--- utils.c
+++ utils.c
@@ -21,18 +21,42 @@ void ball_mechanics(bool goal) {
     }
     if(!goal) {
         // Hits a player.
-        if((((BALL.x+2) == (GK_BLUE.x-X_LEN)) || ((BALL.x-2) == (GK_BLUE.x+X_LEN))) && (BALL.y == GK_BLUE.y))
+        bool check_more = true;
+        if((((BALL.x+2) == (GK_BLUE.x-X_LEN)) || ((BALL.x-2) == (GK_BLUE.x+X_LEN))) && (BALL.y == GK_BLUE.y)) {
             BALL.velocity_x *= -1;
+            check_more = false;
+        }
         else if((((BALL.x+2) == (GK_BLUE.x-X_LEN)) || ((BALL.x-2) == (GK_BLUE.x+X_LEN))) && ((BALL.y >= GK_BLUE.y-Y_LEN) && (BALL.y <= GK_BLUE.y+Y_LEN))) {
             BALL.velocity_x *= -1;     
-            BALL.velocity_y *= -1;       
+            BALL.velocity_y *= -1;    
+            check_more = false;   
         }
-        else if((((BALL.x+2) == (GK_RED.x-X_LEN)) || ((BALL.x-2) == (GK_RED.x+X_LEN))) && (BALL.y == GK_RED.y))
+        else if((((BALL.x+2) == (GK_RED.x-X_LEN)) || ((BALL.x-2) == (GK_RED.x+X_LEN))) && (BALL.y == GK_RED.y)) {
             BALL.velocity_x *= -1;
+            check_more = false;
+        }
         else if((((BALL.x+2) == (GK_RED.x-X_LEN)) || ((BALL.x-2) == (GK_RED.x+X_LEN))) && ((BALL.y >= GK_RED.y-Y_LEN) && (BALL.y <= GK_RED.y+Y_LEN))) {
             BALL.velocity_x *= -1;     
-            BALL.velocity_y *= -1;       
-        }        
+            BALL.velocity_y *= -1; 
+            check_more = false;      
+        }
+        else if(check_more) {
+            int i;
+            for(i = 0; i < 2; i++) {
+                if((((BALL.x+2) == (DEF_BLUE[i].x-X_LEN)) || ((BALL.x-2) == (DEF_BLUE[i].x+X_LEN))) && (BALL.y == DEF_BLUE[i].y)) {
+                    BALL.velocity_x *= -1;
+                    check_more = false;
+                    break;
+                }
+                else if((((BALL.x+2) == (DEF_BLUE[i].x-X_LEN)) || ((BALL.x-2) == (DEF_BLUE[i].x+X_LEN))) && ((BALL.y >= DEF_BLUE[i].y-Y_LEN) && (BALL.y <= DEF_BLUE[i].y+Y_LEN))) {
+                    BALL.velocity_x *= -1;     
+                    BALL.velocity_y *= -1; 
+                    check_more = false;   
+                    break;   
+                }    
+            }
+            if(check_more)
+        }                
         // Hits a boundary.
         if(((BALL.x-2) == 0) || ((BALL.x+2) == X_EDGE))
             BALL.velocity_x *= -1;
